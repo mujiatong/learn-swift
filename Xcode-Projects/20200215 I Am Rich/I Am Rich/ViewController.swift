cf35bf57768e13a7ef7f65204c87eddd8e4bb088
mujiatong <siqi_9@163.com>
2020-02-15 17:04:27 +0800

    Add 20200215 I Am Rich project

---
+++ Xcode-Projects/20200215 I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Alex on 2020/2/15.
+//  Copyright © 2020 Alex. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
